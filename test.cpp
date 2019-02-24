84e7e4666996818afc9dc06cfb6ebcb437fe11cc
yogiarrazy <47936417+yogiarrazy@users.noreply.github.com>
2019-02-24 15:26:24 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-	std::cout<<"Halo saya Bimo, Joshua"<<std::endl;
+	std::cout<<"Halo saya Bimo, Joshua, yogi"<<std::endl;
 	std::cout<<"Senang berkenalan denganmu"<<std::endl;
 	return 0;
 }
